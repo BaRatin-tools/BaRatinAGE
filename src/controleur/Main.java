2f102e74635d7675a83ff93bc9212700baee6c0b
IvanHeriver <ihach_ihach@hotmail.com>
2022-11-24 15:52:49 +0100

    auto reformating

--- src/controleur/Main.java
+++ src/controleur/Main.java
@@ -8,29 +8,43 @@ import javax.swing.UIManager.LookAndFeelInfo;
 import moteur.Station;
 import vue.MainFrame;
 import Utils.Config;
+
 /**
  * Main
+ * 
  * @author Ben Renard, Irstea Lyon
  */
-public class Main{
+public class Main {
+
+	public static String[] parseRow(String row, String sep) {
+
+		return row.split(sep);
+	}
+
 	public static void main(String[] args) {
 		// try setting the look-and-feel
 		try {
-			Config conf= Config.getInstance();
-			if(conf.getLookAndFeel().equals("OS")){UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}
-			else{for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-					if (conf.getLookAndFeel().equals(info.getName())) {UIManager.setLookAndFeel(info.getClassName());break;}
+			Config conf = Config.getInstance();
+			if (conf.getLookAndFeel().equals("OS")) {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} else {
+				for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+					if (conf.getLookAndFeel().equals(info.getName())) {
+						UIManager.setLookAndFeel(info.getClassName());
+						break;
+					}
 				}
 			}
+		} catch (Exception e) {
 		}
-	    catch(Exception e){};// leave to default look-and-feel
-	    // Run the GUI
+		;// leave to default look-and-feel
+			// Run the GUI
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Station.getInstance();
 					MainFrame.getInstance();
-					//UIManager.put("OptionPane.messageFont",GUI.getOptions().getFontLabel());
+					// UIManager.put("OptionPane.messageFont",GUI.getOptions().getFontLabel());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
