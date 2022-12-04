9bfcfafe2791e5bdab08003dc1854ddd36431e84
IvanHeriver <ihach_ihach@hotmail.com>
2022-12-04 10:57:57 +0100

    Removed/added logs

--- src/commons/textFileReader.java
+++ src/commons/textFileReader.java
@@ -1,4 +1,5 @@
 package commons;
+
 import org.mozilla.universalchardet.UniversalDetector;
 
 import java.io.File;
@@ -9,34 +10,33 @@ import java.util.Scanner;
 
 public class textFileReader {
 
-	public static void main (String[] args) throws java.io.IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: java TestDetectorFile FILENAME");
-			System.exit(1);
-		}
-		java.io.File file = new java.io.File(args[0]);
-		String encoding = UniversalDetector.detectCharset(file);
-		if (encoding != null) {
-			System.out.println("Detected encoding = " + encoding);
-		} else {
-			System.out.println("No encoding detected.");
-		}
+	public static void main(String[] args) throws java.io.IOException {
+		// if (args.length != 1) {
+		// System.err.println("Usage: java TestDetectorFile FILENAME");
+		// System.exit(1);
+		// }
+		// java.io.File file = new java.io.File(args[0]);
+		// String encoding = UniversalDetector.detectCharset(file);
+		// if (encoding != null) {
+		// System.out.println("Detected encoding = " + encoding);
+		// } else {
+		// System.out.println("No encoding detected.");
+		// }
 	}
-	
+
 	static public String getFileInfo(File f) throws java.io.IOException {
 		System.out.println(f);
 		String encoding = UniversalDetector.detectCharset(f);
-		if (encoding != null) {
-			System.out.println("Detected encoding = " + encoding);
-		} else {
-			System.out.println("No encoding detected.");
-		}
+		// if (encoding != null) {
+		// System.out.println("Detected encoding = " + encoding);
+		// } else {
+		// System.out.println("No encoding detected.");
+		// }
 		return encoding;
 	}
-	
-	
-	static public Scanner createScanner(File f ) throws IOException {
-//		StandardCharsets.ISO_8859_1;
+
+	static public Scanner createScanner(File f) throws IOException {
+		// StandardCharsets.ISO_8859_1;
 		String encoding = textFileReader.getFileInfo(f);
 		Charset cs = StandardCharsets.UTF_8;
 		if (encoding == "US-ASCII") {
@@ -44,10 +44,7 @@ public class textFileReader {
 		} else if (encoding == "WINDOWS-1252") {
 			cs = StandardCharsets.ISO_8859_1;
 		}
-		System.out.println("Using charset " + cs);
+		// System.out.println("Using charset " + cs);
 		return new Scanner(f, cs);
 	}
 }
-
-
-
