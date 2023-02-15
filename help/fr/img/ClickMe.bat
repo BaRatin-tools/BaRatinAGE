1f67865b58fa658ea60d57791b923f5a7d404d24
IvanHeriver <ihach_ihach@hotmail.com>
2023-02-15 13:56:27 +0100

    FRESH START
    V3 of BaRatinAGE is a complete rewrite of the code using many bits and pieces of version 2.x. There is many reason to start from scratch, the two major one being:
    - it is faster to reuse only what needs to be reused (by grabbing code from version 2.x) and write new code when it is needed.
    - it is also clearer since major change will occur in the project structure itself:  the way the code is organized in classes and sub-classes will drastically be different.

--- help/fr/img/ClickMe.bat
+++
@@ -1,2 +0,0 @@
-:: Remplacer "C:\Program Files\Java\jdk1.8.0_151\jre\bin\java.exe" par le dossier d'installation de Java sur votre machine ::
-"C:\Program Files\Java\jdk1.8.0_151\jre\bin\java.exe" -jar BaRatinAGE.jar
