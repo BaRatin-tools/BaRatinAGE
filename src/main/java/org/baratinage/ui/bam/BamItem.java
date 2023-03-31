9c2734da50283b0e12a29ffd763e4a2e23ed2aed
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-03-31 11:01:10 +0200

    making BamItem resposible for its own header
    BamItem now has specific methods to set its content, title, and actions such as delete action.
    + cleanup

--- src/main/java/org/baratinage/ui/bam/BamItem.java
+++ src/main/java/org/baratinage/ui/bam/BamItem.java
@@ -1,42 +1,55 @@
 package org.baratinage.ui.bam;
 
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.Component;
 import java.util.ArrayList;
-// import java.util.Arrays;
 import java.util.EventListener;
 import java.util.List;
 import java.util.UUID;
-// import java.util.concurrent.SubmissionPublisher;
-// import java.util.concurrent.Flow.Publisher;
-// import java.util.concurrent.Flow.Subscriber;
-// import java.util.concurrent.Flow.Subscription;
 
-// import org.baratinage.ui.container.FlexPanel;
-// import org.baratinage.jbam.BaM;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+
 import org.baratinage.ui.container.RowColPanel;
 
 abstract public class BamItem extends RowColPanel {
 
     private String uuid;
-    // private List<BamItem> parents;
     private BamItem[] parents;
-    // private List<BamItem> children;
 
     @FunctionalInterface
     public interface BamItemChangeListener extends EventListener {
-        public void onUpdate(BamItem item);
+        public void onChange(BamItem item);
     }
 
     private List<BamItemChangeListener> bamItemChangeListeners;
-    // private Publisher<BamItem> publisher;
-    // private List<Subscription> subscriptions;
-    // // private Publisher<BamItem> publisher;
+
+    private JLabel titleLabel;
+    private JButton deleteButton;
+    private RowColPanel headerPanel;
+    private RowColPanel contentPanel;
 
     public BamItem(BamItem... parents) {
-        // super(AXIS.ROW);
         super(AXIS.COL);
-        this.setGap(5);
-        // this.children = new ArrayList<>();
-        // this.parents = new ArrayList<>(Arrays.asList(bamItems));
+
+        headerPanel = new RowColPanel(AXIS.ROW);
+        headerPanel.setGap(5);
+        headerPanel.setPadding(5);
+        contentPanel = new RowColPanel();
+
+        super.appendChild(headerPanel, 0, 0, 0, 0, 0);
+        super.appendChild(new JSeparator(), 0, 0, 0, 0, 0);
+        super.appendChild(contentPanel, 1, 0, 0, 0, 0);
+
+        titleLabel = new JLabel(getName());
+        Font font = titleLabel.getFont();
+        titleLabel.setFont(font.deriveFont(Font.BOLD));
+        deleteButton = new JButton("Supprimer");
+
+        headerPanel.appendChild(titleLabel, 1);
+        headerPanel.appendChild(deleteButton, 0);
 
         this.uuid = UUID.randomUUID().toString();
         this.bamItemChangeListeners = new ArrayList<>();
@@ -49,16 +62,26 @@ abstract public class BamItem extends RowColPanel {
             });
         }
 
-        // this.subscriptions = new ArrayList<>();
-        // this.publisher = new Publisher<>() {
+    }
 
-        // private List<Subscriber<BamItem>> subscribers;
-        // @Override
-        // public void subscribe(Subscriber<BamItem> subscriber) {
-        // this.subscribers.add(subscriber));
-        // }
+    public void addDeleteAction(ActionListener action) {
+        this.deleteButton.addActionListener(action);
+    }
 
-        // }
+    public void setTitle(String title) {
+        this.titleLabel.setText(title);
+    }
+
+    public void setContent(Component component) {
+        this.contentPanel.clear();
+        this.contentPanel.appendChild(component);
+    }
+
+    @Override
+    public void appendChild(Component component, double weight,
+            int topPadding, int rightPadding,
+            int bottomPadding, int leftPadding) {
+        throw new UnsupportedOperationException("Use setContent method! AppendChild is disabled for BamItem");
     }
 
     public String getUUID() {
@@ -66,30 +89,6 @@ abstract public class BamItem extends RowColPanel {
     }
 
     // // FIXME: when/how are children removed?
-    // public void addChild(BamItem child) {
-    // children.add(child);
-    // // subscriptions.add(new Subscription() {
-
-    // // @Override
-    // // public void request(long n) {
-
-    // // System.out.println("ON_REQUEST");
-    // // }
-
-    // // @Override
-    // // public void cancel() {
-    // // System.out.println("ON_CANCEL");
-    // // }
-
-    // // });
-
-    // // child.sub
-    // }
-
-    // public void addParent(BamItem parent) {
-    // parents.add(parent);
-    // }
-
     public void hasChanged() {
         fireChangeListeners();
         // for (BamItem child : this.children) {
@@ -107,48 +106,9 @@ abstract public class BamItem extends RowColPanel {
 
     public void fireChangeListeners() {
         for (BamItemChangeListener listener : this.bamItemChangeListeners) {
-            listener.onUpdate(this);
+            listener.onChange(this);
         }
     }
-
-    // @Override
-    // public void subscribe(Subscriber<BamItem> subscriber) {
-
-    // }
-
-    // @Override
-    // public void subscribe<T>(Subscriber<T extends BamItem> sub) {
-    // System.out.println("ON_COMPLETE");
-    // }
-
-    // private void subscribe(BamItem item) {
-    // // called when a child wants to be kept informed of any change
-
-    // }
-
-    // these four methods are used to follow other BamItem this bamItem depends on
-
-    // @Override
-    // public void onComplete() {
-    // System.out.println("ON_COMPLETE");
-    // }
-
-    // @Override
-    // public void onError(Throwable throwable) {
-    // System.out.println("ON_ERROR");
-    // throwable.printStackTrace();
-    // }
-
-    // @Override
-    // public void onNext(BamItem item) {
-    // System.out.println("ON_NEXT");
-    // }
-
-    // @Override
-    // public void onSubscribe(Subscription subscription) {
-    // System.out.println("ON_SUBSCRIBE");
-    // this.subscriptions.add(subscription);
-    // }
 
     public abstract String getName();
 
