c88519d2fcc10f6d8458a018c6a9b4c5e05e1952
IvanHeriver <ihach_ihach@hotmail.com>
2023-03-31 16:00:56 +0200

    First implementation of HydraulicControl

--- src/main/java/org/baratinage/ui/baratin/HydraulicControls.java
+++ src/main/java/org/baratinage/ui/baratin/HydraulicControls.java
@@ -10,22 +10,16 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
 import javax.swing.ListSelectionModel;
 
+import org.baratinage.ui.component.NumberField;
 import org.baratinage.ui.container.GridPanel;
 import org.baratinage.ui.container.RowColPanel;
 
 public class HydraulicControls extends RowColPanel {
 
-    // JList listOfControls;
-    // JListMo
-
-    int numberOfControls;
-
-    // List<ExplorerItem> controls;
-    // Explorer controlExplorer;
-    // List<ExplorerItem> controls;
     private record ControlItem(String id, String label, String icon) {
         public String toString() {
             return label();
@@ -35,7 +29,6 @@ public class HydraulicControls extends RowColPanel {
     JList<ControlItem> controlSelector;
     DefaultListModel<ControlItem> controlSelectorModel;
 
-    // A<String> controlSelector;
     List<HydraulicControl> controls;
     RowColPanel currentControl;
 
@@ -44,34 +37,35 @@ public class HydraulicControls extends RowColPanel {
     public HydraulicControls() {
         super(AXIS.COL);
 
-        // NumberField field = new NumberField();
-        // this.appendChild(field);
-
         controls = new ArrayList<>();
 
-        // controlExplorer = new Explorer("");
         controlSelector = new JList<>();
         controlSelector.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         controlSelector.addListSelectionListener(e -> {
             if (!e.getValueIsAdjusting()) {
-                int selectedIndex = controlSelector.getSelectedIndex();
-                System.out.println("Selection is " + selectedIndex);
-                currentControl.clear();
-                if (selectedIndex >= 0 && selectedIndex < controls.size()) {
-                    currentControl.appendChild(controls.get(selectedIndex));
-
+                ControlItem controlItem = controlSelector.getSelectedValue();
+                if (controlItem != null) {
+                    controls.stream()
+                            .filter(ctrl -> ctrl.id.equals(controlItem.id()))
+                            .findFirst()
+                            .ifPresentOrElse((ctrl) -> {
+                                currentControl.clear();
+                                currentControl.appendChild(ctrl);
+                                currentControl.updateUI();
+                            }, () -> {
+                                currentControl.clear();
+                                currentControl.updateUI();
+                            });
+                } else {
+                    currentControl.clear();
+                    currentControl.updateUI();
                 }
-                // splitPaneContainer.updateUI();
+
             }
         });
         controlSelectorModel = new DefaultListModel<>();
         controlSelector.setModel(controlSelectorModel);
 
-        // controlExplorer.appendItem(new ExplorerItem(TOOL_TIP_TEXT_KEY,
-        // TOOL_TIP_TEXT_KEY, TOOL_TIP_TEXT_KEY))
-
-        // EmptyBorder eb = new EmptyBorder(new Insets(0, 0, 0, 0));
-        // controlSelector.setBorder(eb);
         currentControl = new RowColPanel();
 
         RowColPanel listPanel = new RowColPanel(AXIS.COL);
@@ -82,60 +76,86 @@ public class HydraulicControls extends RowColPanel {
         listPanel.appendChild(controlSelector);
         JSplitPane splitPaneContainer = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         splitPaneContainer.setBorder(BorderFactory.createEmptyBorder());
-        // splitPaneContainer = new SplitPanel(SplitPanel.DIR.H);
-        // splitPaneContainer.setLeftComponent(controlExplorer);
-        // splitPaneContainer.setLeftComponent(controlSelector);
+
         splitPaneContainer.setLeftComponent(listScrollContainer);
         splitPaneContainer.setRightComponent(currentControl);
-
-        // splitPaneContainer.setResizeWeight(0.5);
-
-        // controls = new ArrayList<>();
-
-        // for (int k = 0; k < 30; k++) {
-        // // controls.add(new ExplorerItem(
-        // // "control_" + (k + 1),
-        // // "control_" + (k + 1),
-        // // null));
-        // controlIdLabel ctrl = new controlIdLabel(UUID.randomUUID().toString(),
-        // "Contrôle #" + (k + 1), null);
-        // controlSelectorModel.addElement(ctrl);
-        // controls.add(new HydraulicControl(ctrl.id(), ctrl.label()));
-        // }
-
-        // for (ExplorerItem control : controls) {
-        // controlExplorer.appendItem(control);
-        // }
 
         this.appendChild(splitPaneContainer);
 
     }
 
-    public void setCurrentControl(int index) {
-        // if (index <w)
-
-    }
-
     public void setNumberOfControls(int n) {
-
-        controlSelectorModel.clear();
-        controls.clear();
-        for (int k = 0; k < n; k++) {
-            ControlItem ctrl = new ControlItem(UUID.randomUUID().toString(),
-                    "Contrôle #" + (k + 1), null);
-            controlSelectorModel.addElement(ctrl);
-            controls.add(new HydraulicControl(ctrl.id(), ctrl.label()));
+        int m = controlSelectorModel.size();
+        if (m > n) {
+            int d = m - n;
+            System.out.println("Deleting the " + d + " controls no longer needed");
+            for (int k = 0; k < d; k++) {
+                controlSelectorModel.remove(m - k - 1);
+            }
+        } else if (m < n) {
+            // int d = n - m;q
+            for (int k = m; k < n; k++) {
+                ControlItem ctrl = new ControlItem(UUID.randomUUID().toString(),
+                        "Contrôle #" + (k + 1), null);
+                controlSelectorModel.addElement(ctrl);
+                controls.add(new HydraulicControl(ctrl.id(), ctrl.label()));
+            }
+        } else {
+            System.out.println("Number of controls are already matching!");
         }
     }
 
-    private class HydraulicControl extends GridPanel {
+    private class HydraulicControl extends RowColPanel {
         public final String id;
-        private String label;
+
+        private GridPanel parametersPanel;
+        private NumberField activationStage;
+        private NumberField activationStageUncertainty;
+        private NumberField coefficient;
+        private NumberField coefficientUncertainty;
+        private NumberField exponent;
+        private NumberField exponentUncertainty;
 
         public HydraulicControl(String id, String label) {
+            super(AXIS.COL);
             this.id = id;
-            this.label = label;
-            this.insertChild(new JLabel(label), 0, 0);
+
+            this.appendChild(new JLabel(label), 0, 5);
+            this.appendChild(new JSeparator(), 0);
+
+            parametersPanel = new GridPanel();
+            parametersPanel.setAnchor(ANCHOR.N);
+            parametersPanel.setGap(5);
+            parametersPanel.setPadding(5);
+            parametersPanel.setColWeight(1, 1);
+            parametersPanel.setColWeight(2, 1);
+
+            parametersPanel.insertChild(new JLabel("<html>Valeur <i>a priori</i></html> "), 1, 0);
+            parametersPanel.insertChild(new JLabel("+/- (Incertitude élargie)"), 2, 0);
+
+            JLabel activationStageLabel = new JLabel("k - Hauteur d'activation");
+            activationStage = new NumberField();
+            activationStageUncertainty = new NumberField();
+            parametersPanel.insertChild(activationStageLabel, 0, 1);
+            parametersPanel.insertChild(activationStage, 1, 1);
+            parametersPanel.insertChild(activationStageUncertainty, 2, 1);
+
+            JLabel coefficientLabel = new JLabel("a - Coefficient");
+            coefficient = new NumberField();
+            coefficientUncertainty = new NumberField();
+            parametersPanel.insertChild(coefficientLabel, 0, 2);
+            parametersPanel.insertChild(coefficient, 1, 2);
+            parametersPanel.insertChild(coefficientUncertainty, 2, 2);
+
+            JLabel exponentLabel = new JLabel("c - Exposant");
+            exponent = new NumberField();
+            exponentUncertainty = new NumberField();
+            parametersPanel.insertChild(exponentLabel, 0, 3);
+            parametersPanel.insertChild(exponent, 1, 3);
+            parametersPanel.insertChild(exponentUncertainty, 2, 3);
+
+            this.appendChild(parametersPanel);
+
         }
     }
 
