88f6a1e51e183c76b086517948b528ecd4fb0671
IvanHeriver <ihach_ihach@hotmail.com>
2023-04-02 14:57:08 +0200

    using the new reactivity mechanism

--- src/main/java/org/baratinage/ui/baratin/HydraulicConfiguration.java
+++ src/main/java/org/baratinage/ui/baratin/HydraulicConfiguration.java
@@ -1,7 +1,6 @@
 package org.baratinage.ui.baratin;
 
 import javax.swing.BorderFactory;
-import javax.swing.JLabel;
 import javax.swing.JSplitPane;
 
 import org.baratinage.jbam.Parameter;
@@ -11,59 +10,41 @@ import org.baratinage.jbam.Parameter;
 import org.baratinage.ui.bam.BamItem;
 import org.baratinage.ui.bam.IModelDefinition;
 import org.baratinage.ui.bam.IPriors;
-import org.baratinage.ui.component.TextField;
-import org.baratinage.ui.container.GridPanel;
-import org.baratinage.ui.container.RowColPanel;
 
-class HydraulicConfiguration extends BamItem
+class HydraulicConfiguration extends BaRatinItem
         implements IModelDefinition, IPriors {
 
+    public static final int TYPE = (int) Math.floor(Math.random() * Integer.MAX_VALUE);
     static private final String defaultNameTemplate = "Configuration Hydraulique #%s";
     static private int nInstance = 0;
 
-    private String name;
-    private String description;
-
     private ControlMatrix controlMatrix;
-
     private HydraulicControls hydraulicControls;
 
     public HydraulicConfiguration() {
-        super();
-        HydraulicConfiguration.nInstance++;
-        this.name = String.format(
-                HydraulicConfiguration.defaultNameTemplate,
-                HydraulicConfiguration.nInstance + "");
-        this.description = "";
-        System.out.println(description);
+        super(TYPE);
 
-        GridPanel header = new GridPanel();
-        header.setGap(5);
-        header.setPadding(5);
-        header.setRowWeight(0, 1);
-        header.setColWeight(1, 1);
+        nInstance++;
+        setName(String.format(
+                defaultNameTemplate,
+                nInstance + ""));
+        setDescription("");
 
-        JLabel nameFieldLabel = new JLabel("Nom de la configuration hydraulique");
-        TextField nameField = new TextField();
-        nameField.addChangeListener(nt -> {
-            setName(nt);
-            hasChanged();
-        });
-        nameField.setText(this.name);
-        JLabel descFieldLabel = new JLabel("Description");
-        TextField descField = new TextField();
-
-        header.insertChild(nameFieldLabel, 0, 0);
-        header.insertChild(nameField, 1, 0);
-        header.insertChild(descFieldLabel, 0, 1);
-        header.insertChild(descField, 1, 1);
+        setNameFieldLabel("Nom de la configuration hydraulique");
+        setDescriptionFieldLabel("Description de la configuration hydraulique");
 
         controlMatrix = new ControlMatrix();
-        controlMatrix.addChangeListener(matrix -> {
+        controlMatrix.addFollower((o) -> {
             System.out.println("Control matrix has changed!");
-            updateControls(matrix);
+            hasChanged();
+            updateControls(controlMatrix.getControlMatrix());
         });
+
         hydraulicControls = new HydraulicControls();
+        hydraulicControls.addFollower((o) -> {
+            System.out.println("Hydraulic controls have changed!");
+            hasChanged();
+        });
 
         JSplitPane splitPaneContainer = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
         splitPaneContainer.setBorder(BorderFactory.createEmptyBorder());
@@ -71,25 +52,15 @@ class HydraulicConfiguration extends BamItem
         splitPaneContainer.setRightComponent(hydraulicControls);
         splitPaneContainer.setResizeWeight(0.5);
 
-        RowColPanel content = new RowColPanel(RowColPanel.AXIS.COL);
-        content.appendChild(header, 0);
-        content.appendChild(splitPaneContainer, 1);
-        content.setGap(5);
-
-        setContent(content);
+        setContent(splitPaneContainer);
 
         updateControls(controlMatrix.getControlMatrix());
+
     }
 
     private void updateControls(boolean[][] controlMatrix) {
-        ControlMatrix.printMatrix(controlMatrix);
         hydraulicControls.setNumberOfControls(controlMatrix.length);
 
-    }
-
-    public void setName(String name) {
-        setTitle(name);
-        this.name = name;
     }
 
     @Override
@@ -118,11 +89,6 @@ class HydraulicConfiguration extends BamItem
     }
 
     @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
     public String getXtra(String workspace) {
         boolean[][] matrix = this.controlMatrix.getControlMatrix();
         String xtra = "";
@@ -133,6 +99,11 @@ class HydraulicConfiguration extends BamItem
             xtra += "\n";
         }
         return xtra;
+    }
+
+    @Override
+    public Parameter[] getParameters() {
+        return hydraulicControls.getParameters();
     }
 
     @Override
@@ -151,11 +122,6 @@ class HydraulicConfiguration extends BamItem
     public void fromJsonString(String jsonString) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'fromJsonString'");
-    }
-
-    @Override
-    public Parameter[] getParameters() {
-        return hydraulicControls.getParameters();
     }
 
 }
