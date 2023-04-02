b76d55b6586e3289b0f9d22b3f06cdd9f0be9b09
IvanHeriver <ihach_ihach@hotmail.com>
2023-04-02 14:55:25 +0200

    restructurated baratin project

--- src/main/java/org/baratinage/ui/baratin/BaratinProject.java
+++ src/main/java/org/baratinage/ui/baratin/BaratinProject.java
@@ -1,14 +1,12 @@
 package org.baratinage.ui.baratin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
 
 import org.baratinage.ui.bam.BamItem;
+import org.baratinage.ui.bam.BamItemList;
 import org.baratinage.ui.component.Explorer;
 import org.baratinage.ui.component.ExplorerItem;
 // import org.baratinage.ui.component.ImportedData;
@@ -35,22 +33,16 @@ import org.baratinage.ui.container.RowColPanel;
 
 public class BaratinProject extends RowColPanel {
 
-    private record ProjectBamItem(BamItem bamItem, ExplorerItem explorerItem) {
-    }
+    // private record ProjectBamItem(BamItem bamItem, ExplorerItem explorerItem) {
+    // }
 
-    private List<ProjectBamItem> hydraulicConfigs;
-    // private List<ImportedData> importedData;
-    // private List<Gaugings> gaugings;
-    // private List<Limnigraph> limnigraphs;
-    // private List<StructuralError> structuralErrors;
-    // private List<PosteriorRatingCurve> posteriorRatingCurve;
-    // private List<Hydrograph> hydrographs;
-    // private List<Ui
+    // private List<BamItem> items;
+    private BamItemList items;
 
     private ExplorerItem hydraulicConfig;
     private ExplorerItem gaugings;
     private ExplorerItem structuralError;
-    private ExplorerItem posteriorRatingCurve;
+    private ExplorerItem ratingCurve;
 
     static private final String hydroConfigIconPath = "./resources/icons/Hydraulic_icon.png";
     static private final String gaugingsIconPath = "./resources/icons/Gauging_icon.png";
@@ -66,22 +58,28 @@ public class BaratinProject extends RowColPanel {
     public BaratinProject() {
         super(AXIS.COL);
 
-        this.hydraulicConfigs = new ArrayList<>();
-
+        this.items = new BamItemList();
         this.actionBar = new RowColPanel(AXIS.ROW, ALIGN.START, ALIGN.STRETCH);
         this.actionBar.setPadding(5);
         this.actionBar.setGap(5);
         this.appendChild(this.actionBar, 0);
         JButton btnNewHydraulicConfig = new JButton();
         btnNewHydraulicConfig.setText("Nouvelle configuration hydraulique");
-        btnNewHydraulicConfig.setIcon(new NoScalingIcon("./resources/icons/Hydraulic_icon.png"));
+        btnNewHydraulicConfig.setIcon(new NoScalingIcon(hydroConfigIconPath));
         btnNewHydraulicConfig.addActionListener(e -> {
             System.out.println("new hydrau config");
             addHydraulicConfig();
         });
-
         this.actionBar.appendChild(btnNewHydraulicConfig);
-        this.actionBar.appendChild(new JButton("action 2"));
+
+        JButton btnNewRatingCurve = new JButton();
+        btnNewRatingCurve.setText("Nouvelle courbe de tarage");
+        btnNewRatingCurve.setIcon(new NoScalingIcon(ratingCurveIconPath));
+        btnNewRatingCurve.addActionListener(e -> {
+            System.out.println("new rating curve");
+            addRatingCurve();
+        });
+        this.actionBar.appendChild(btnNewRatingCurve);
 
         this.content = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
         this.content.setBorder(BorderFactory.createEmptyBorder());
@@ -108,13 +106,12 @@ public class BaratinProject extends RowColPanel {
     private void setupExplorer() {
 
         this.explorer.addTreeSelectionListener(e -> {
-            ExplorerItem item = explorer.getLastSelectedPathComponent();
-            if (item != null) {
-                System.out.println(item.id);
-                ProjectBamItem projBamItem = findProjectBamItem(item.id);
-                if (projBamItem != null) {
+            ExplorerItem explorerItem = explorer.getLastSelectedPathComponent();
+            if (explorerItem != null) {
+                BamItem bamItem = findBamItem(explorerItem.id);
+                if (bamItem != null) {
                     this.currentPanel.clear();
-                    this.currentPanel.appendChild(projBamItem.bamItem, 1);
+                    this.currentPanel.appendChild(bamItem, 1);
 
                 } else {
                     this.currentPanel.clear();
@@ -141,54 +138,94 @@ public class BaratinProject extends RowColPanel {
                 structuralErrIconPath);
         this.explorer.appendItem(structuralError);
 
-        posteriorRatingCurve = new ExplorerItem(
+        ratingCurve = new ExplorerItem(
                 "rc",
-                "<html>Courbes de tarage <i>(a posteriori)</i></html>",
+                "Courbes de tarage",
                 ratingCurveIconPath);
-        this.explorer.appendItem(posteriorRatingCurve);
+        this.explorer.appendItem(ratingCurve);
 
     }
 
-    private void addHydraulicConfig() {
-        HydraulicConfiguration bamItem = new HydraulicConfiguration();
-        bamItem.addChangeListener((i) -> {
-            ProjectBamItem pbi = findProjectBamItem(i.getUUID());
-            String newName = pbi.bamItem.getName();
+    // FIXME: should have an category id as input instead of relying on different
+    // list object to store the different types of items...
+    private void addItem(BamItem bamItem, ExplorerItem explorerItem) {
+
+        // ProjectBamItem item = new ProjectBamItem(bamItem, explorerItem);
+
+        items.add(bamItem);
+        bamItem.setSiblings(items);
+
+        // bamItem.addChangeListener(() -> {
+        // // BamItem bi = findBamItem(i.getUUID());
+        // // String newName = bi.getName();
+        // String newName = bamItem.getName();
+        // if (newName.equals("")) {
+        // newName = "<html><div style='color: red; font-style: italic'>Sans
+        // nom</div></html>";
+        // }
+        // explorerItem.label = newName;
+        // explorer.updateItemView(explorerItem);
+        // });
+        bamItem.addFollower(o -> {
+            String newName = bamItem.getName();
             if (newName.equals("")) {
                 newName = "<html><div style='color: red; font-style: italic'>Sans nom</div></html>";
             }
-            pbi.explorerItem.label = newName;
-            explorer.updateItemView(pbi.explorerItem);
+            explorerItem.label = newName;
+            explorer.updateItemView(explorerItem);
         });
         bamItem.addDeleteAction(e -> {
             System.out.println("Delete " + bamItem.getName());
-            deleteHydraulicConfig(bamItem.getUUID());
+            deleteItem(bamItem, explorerItem);
         });
-        ExplorerItem explorerItem = new ExplorerItem(
-                bamItem.getUUID(),
-                bamItem.getName(),
-                hydroConfigIconPath,
-                hydraulicConfig);
-        hydraulicConfigs.add(new ProjectBamItem(bamItem, explorerItem));
+
         this.explorer.appendItem(explorerItem);
         this.explorer.expandItem(hydraulicConfig);
         this.explorer.selectItem(explorerItem);
+
     }
 
-    private void deleteHydraulicConfig(String id) {
-        ProjectBamItem item = findProjectBamItem(id);
-        hydraulicConfigs.remove(item);
-        this.explorer.removeItem(item.explorerItem);
-        this.explorer.selectItem(item.explorerItem.parentItem);
+    // private void deleteItem(String id) {
+    private void deleteItem(BamItem bamItem, ExplorerItem explorerItem) {
+        // BamItem item = findBamItem(id);
+        items.remove(bamItem);
+        this.explorer.removeItem(explorerItem);
+        this.explorer.selectItem(explorerItem.parentItem);
     }
 
-    private ProjectBamItem findProjectBamItem(String id) {
-        for (ProjectBamItem pbi : this.hydraulicConfigs) {
-            if (pbi.bamItem.getUUID().equals(id)) {
-                return pbi;
+    private void addHydraulicConfig() {
+        HydraulicConfiguration hydroConf = new HydraulicConfiguration();
+        ExplorerItem explorerItem = new ExplorerItem(
+                hydroConf.getUUID(),
+                hydroConf.getName(),
+                hydroConfigIconPath,
+                hydraulicConfig);
+        addItem(hydroConf, explorerItem);
+
+    }
+
+    private void addRatingCurve() {
+        RatingCurve ratingCurveItem = new RatingCurve();
+        ExplorerItem explorerItem = new ExplorerItem(
+                ratingCurveItem.getUUID(),
+                ratingCurveItem.getName(),
+                ratingCurveIconPath,
+                ratingCurve);
+        addItem(ratingCurveItem, explorerItem);
+
+    }
+
+    private BamItem findBamItem(String id) {
+        for (BamItem item : this.items) {
+            if (item.getUUID().equals(id)) {
+                return item;
             }
         }
         return null;
     }
+
+    // private ExplorerItem findExplorerItem(String id) {
+
+    // }
 
 }
