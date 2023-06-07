fb232e71010654acc333474239b6b24de65dcffe
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-06-07 09:40:15 +0200

    cleanup

--- src/main/java/org/baratinage/ui/bam/RunBamPost.java
+++ src/main/java/org/baratinage/ui/bam/RunBamPost.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import org.baratinage.jbam.BaM;
 import org.baratinage.jbam.CalDataResidualConfig;
 import org.baratinage.jbam.CalibrationConfig;
-import org.baratinage.jbam.CalibrationData;
 import org.baratinage.jbam.McmcConfig;
 import org.baratinage.jbam.McmcCookingConfig;
 import org.baratinage.jbam.McmcSummaryConfig;
@@ -18,72 +17,72 @@ import org.baratinage.jbam.StructuralErrorModel;
 
 public class RunBamPost extends RunBam {
 
-    public void configure(
-            String workspace,
-            IModelDefinition modelDefinition,
-            IPriors priors,
-            IStructuralError structuralError,
-            ICalibrationData calibrationData,
-            IPredictionExperiment[] predictionExperiments) {
+        public void configure(
+                        String workspace,
+                        IModelDefinition modelDefinition,
+                        IPriors priors,
+                        IStructuralError structuralError,
+                        ICalibrationData calibrationData,
+                        IPredictionExperiment[] predictionExperiments) {
 
-        this.workspace = Path.of(workspace);
+                this.workspace = Path.of(workspace);
 
-        String xTra = modelDefinition.getXtra(workspace);
+                String xTra = modelDefinition.getXtra(workspace);
 
-        Parameter[] parameters = priors.getParameters();
+                Parameter[] parameters = priors.getParameters();
 
-        String[] inputNames = modelDefinition.getInputNames();
-        String[] outputNames = modelDefinition.getOutputNames();
+                String[] inputNames = modelDefinition.getInputNames();
+                String[] outputNames = modelDefinition.getOutputNames();
 
-        Model model = new Model(
-                modelDefinition.getModelId(),
-                inputNames.length,
-                outputNames.length,
-                parameters,
-                xTra);
+                Model model = new Model(
+                                modelDefinition.getModelId(),
+                                inputNames.length,
+                                outputNames.length,
+                                parameters,
+                                xTra);
 
-        StructuralErrorModel linearErrModel = structuralError.getStructuralErrorModel();
+                StructuralErrorModel linearErrModel = structuralError.getStructuralErrorModel();
 
-        ModelOutput[] modelOutputs = new ModelOutput[outputNames.length];
-        for (int k = 0; k < outputNames.length; k++) {
-            modelOutputs[k] = new ModelOutput(outputNames[k], linearErrModel);
+                ModelOutput[] modelOutputs = new ModelOutput[outputNames.length];
+                for (int k = 0; k < outputNames.length; k++) {
+                        modelOutputs[k] = new ModelOutput(outputNames[k], linearErrModel);
+                }
+
+                CalDataResidualConfig calDataResidualConfig = new CalDataResidualConfig();
+
+                McmcCookingConfig mcmcCookingConfig = new McmcCookingConfig();
+                McmcSummaryConfig mcmcSummaryConfig = new McmcSummaryConfig();
+                // FIXME: a IMcmc should be an argument that provides the MCMC configuration
+                McmcConfig mcmcConfig = new McmcConfig();
+
+                CalibrationConfig fakeCalibrationConfig = new CalibrationConfig(
+                                model,
+                                modelOutputs,
+                                calibrationData.getCalibrationData(),
+                                mcmcConfig,
+                                mcmcCookingConfig,
+                                mcmcSummaryConfig,
+                                calDataResidualConfig);
+
+                RunOptions runOptions = new RunOptions(
+                                true,
+                                true,
+                                true,
+                                true);
+
+                PredictionConfig[] predConfigs = new PredictionConfig[predictionExperiments.length];
+                for (int k = 0; k < predictionExperiments.length; k++) {
+                        predConfigs[k] = predictionExperiments[k].getPredictionConfig();
+                }
+
+                bam = new BaM(
+                                fakeCalibrationConfig,
+                                predConfigs,
+                                runOptions,
+                                null,
+                                null);
+
+                isConfigured = true;
         }
-
-        CalDataResidualConfig calDataResidualConfig = new CalDataResidualConfig();
-
-        McmcCookingConfig mcmcCookingConfig = new McmcCookingConfig();
-        McmcSummaryConfig mcmcSummaryConfig = new McmcSummaryConfig();
-        // FIXME: a IMcmc should be an argument that provides the MCMC configuration
-        McmcConfig mcmcConfig = new McmcConfig();
-
-        CalibrationConfig fakeCalibrationConfig = new CalibrationConfig(
-                model,
-                modelOutputs,
-                calibrationData.getCalibrationData(),
-                mcmcConfig,
-                mcmcCookingConfig,
-                mcmcSummaryConfig,
-                calDataResidualConfig);
-
-        RunOptions runOptions = new RunOptions(
-                true,
-                true,
-                true,
-                true);
-
-        PredictionConfig[] predConfigs = new PredictionConfig[predictionExperiments.length];
-        for (int k = 0; k < predictionExperiments.length; k++) {
-            predConfigs[k] = predictionExperiments[k].getPredictionConfig();
-        }
-
-        bam = new BaM(
-                fakeCalibrationConfig,
-                predConfigs,
-                runOptions,
-                null,
-                null);
-
-        isConfigured = true;
-    }
 
 }
