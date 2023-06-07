05e9fa6235998a3fb464ee92c123d101e14c8105
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-06-07 10:49:30 +0200

    Create WriteFile.java

---
+++ src/main/java/org/baratinage/utils/WriteFile.java
@@ -0,0 +1,8 @@
+package org.baratinage.utils;
+
+import org.baratinage.jbam.utils.Write;
+
+// FIXME: no need to reimplement all the methods already implement in jbam package for that purpose
+public class WriteFile extends Write {
+
+}
