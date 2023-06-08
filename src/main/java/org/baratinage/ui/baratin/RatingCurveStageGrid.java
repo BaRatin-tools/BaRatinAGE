d8fc2fca800be3fab37ec0427848b7d505f3e815
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-06-08 11:03:25 +0200

    cleanup

--- src/main/java/org/baratinage/ui/baratin/RatingCurveStageGrid.java
+++ src/main/java/org/baratinage/ui/baratin/RatingCurveStageGrid.java
@@ -118,8 +118,6 @@ public class RatingCurveStageGrid extends RowColPanel implements IPredictionData
     }
 
     public void setMinValue(double value) {
-        // stageGridConfig = new StageGridConfig(value, stageGridConfig.max,
-        // stageGridConfig.step);
         minStageField.setValue(value);
         minStageField.updateTextField();
         updateStageGridConfig();
@@ -130,8 +128,6 @@ public class RatingCurveStageGrid extends RowColPanel implements IPredictionData
     }
 
     public void setMaxValue(double value) {
-        // stageGridConfig = new StageGridConfig(stageGridConfig.min, value,
-        // stageGridConfig.step);
         maxStageField.setValue(value);
         maxStageField.updateTextField();
         updateStageGridConfig();
@@ -142,24 +138,10 @@ public class RatingCurveStageGrid extends RowColPanel implements IPredictionData
     }
 
     public void setStepValue(double value) {
-        // stageGridConfig = new StageGridConfig(stageGridConfig.min,
-        // stageGridConfig.max, value);
         valStepField.setValue(value);
         valStepField.updateTextField();
         updateStageGridConfig();
     }
-
-    // public StageGridConfig getStageGridConfig() {
-    // return stageGridConfig;
-    // }
-
-    // public void setStageGridConfig(StageGridConfig stagegridConfig) {
-    // this.stageGridConfig = new StageGridConfig(
-    // stageGridConfig.min(),
-    // stageGridConfig.max(),
-    // stageGridConfig.step());
-
-    // }
 
     @Override
     public PredictionInput[] getPredictionInputs() {
