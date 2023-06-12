a0bdcb9c4c30add6cb36046424cc539017dd0696
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-06-12 07:29:06 +0200

    cleanup

--- src/main/java/org/baratinage/ui/bam/RunBamPrior.java
+++ src/main/java/org/baratinage/ui/bam/RunBamPrior.java
@@ -30,142 +30,90 @@ import org.baratinage.ui.commons.DefaultStructuralErrorProvider;
 
 public class RunBamPrior extends RunBam {
 
-    public void configure(
-            String workspace,
-            IModelDefinition modelDefinition,
-            IPriors priors,
-            PriorPredictionExperiment[] priorPredictionExperiment
+        public void configure(
+                        String workspace,
+                        IModelDefinition modelDefinition,
+                        IPriors priors,
+                        PriorPredictionExperiment[] priorPredictionExperiment
 
-    ) {
+        ) {
 
-        this.workspace = Path.of(workspace);
+                this.workspace = Path.of(workspace);
 
-        String xTra = modelDefinition.getXtra(workspace);
+                String xTra = modelDefinition.getXtra(workspace);
 
-        Parameter[] parameters = priors.getParameters();
+                Parameter[] parameters = priors.getParameters();
 
-        String[] inputNames = modelDefinition.getInputNames();
-        String[] outputNames = modelDefinition.getOutputNames();
+                String[] inputNames = modelDefinition.getInputNames();
+                String[] outputNames = modelDefinition.getOutputNames();
 
-        Model model = new Model(
-                modelDefinition.getModelId(),
-                inputNames.length,
-                outputNames.length,
-                parameters,
-                xTra);
+                Model model = new Model(
+                                modelDefinition.getModelId(),
+                                inputNames.length,
+                                outputNames.length,
+                                parameters,
+                                xTra);
 
-        // we can use only one default error model since prior predictions
-        // never (?) propagate structural errors
-        IStructuralError structuralError = new DefaultStructuralErrorProvider(
-                DefaultStructuralErrorProvider.TYPE.LINEAR);
-        StructuralErrorModel linearErrModel = structuralError.getStructuralErrorModel();
+                // we can use only one default error model since prior predictions
+                // never (?) propagate structural errors
+                IStructuralError structuralError = new DefaultStructuralErrorProvider(
+                                DefaultStructuralErrorProvider.TYPE.LINEAR);
+                StructuralErrorModel linearErrModel = structuralError.getStructuralErrorModel();
 
-        ModelOutput[] modelOutputs = new ModelOutput[outputNames.length];
-        for (int k = 0; k < outputNames.length; k++) {
-            modelOutputs[k] = new ModelOutput(outputNames[k], linearErrModel);
+                ModelOutput[] modelOutputs = new ModelOutput[outputNames.length];
+                for (int k = 0; k < outputNames.length; k++) {
+                        modelOutputs[k] = new ModelOutput(outputNames[k], linearErrModel);
+                }
+
+                // Creating fake calibration data to satisfy BaM requirements
+                double[] fakeCalibrationData = new double[] { 0 };
+                UncertainData[] inputs = new UncertainData[inputNames.length];
+                for (int k = 0; k < inputNames.length; k++) {
+                        inputs[k] = new UncertainData(inputNames[k], fakeCalibrationData);
+                }
+                UncertainData[] outputs = new UncertainData[inputNames.length];
+                for (int k = 0; k < outputNames.length; k++) {
+                        outputs[k] = new UncertainData(outputNames[k], fakeCalibrationData);
+                }
+
+                CalibrationData calibrationData = new CalibrationData("fakeCalibrationData",
+                                inputs, outputs);
+
+                CalDataResidualConfig calDataResidualConfig = new CalDataResidualConfig();
+
+                McmcCookingConfig mcmcCookingConfig = new McmcCookingConfig();
+                McmcSummaryConfig mcmcSummaryConfig = new McmcSummaryConfig();
+                // FIXME: a IMcmc should be an argument that provides the MCMC configuration
+                McmcConfig mcmcConfig = new McmcConfig();
+
+                CalibrationConfig fakeCalibrationConfig = new CalibrationConfig(
+                                model,
+                                modelOutputs,
+                                calibrationData,
+                                mcmcConfig,
+                                mcmcCookingConfig,
+                                mcmcSummaryConfig,
+                                calDataResidualConfig);
+
+                RunOptions runOptions = new RunOptions(
+                                true,
+                                true,
+                                true,
+                                true);
+
+                PredictionConfig[] predConfigs = new PredictionConfig[priorPredictionExperiment.length];
+                for (int k = 0; k < priorPredictionExperiment.length; k++) {
+                        predConfigs[k] = priorPredictionExperiment[k].getPredictionConfig();
+                }
+
+                bam = new BaM(
+                                fakeCalibrationConfig,
+                                predConfigs,
+                                runOptions,
+                                null,
+                                null);
+
+                isConfigured = true;
         }
-
-        // Creating fake calibration data to satisfy BaM requirements
-        double[] fakeCalibrationData = new double[] { 0 };
-        UncertainData[] inputs = new UncertainData[inputNames.length];
-        for (int k = 0; k < inputNames.length; k++) {
-            inputs[k] = new UncertainData(inputNames[k], fakeCalibrationData);
-        }
-        UncertainData[] outputs = new UncertainData[inputNames.length];
-        for (int k = 0; k < outputNames.length; k++) {
-            outputs[k] = new UncertainData(outputNames[k], fakeCalibrationData);
-        }
-
-        CalibrationData calibrationData = new CalibrationData("fakeCalibrationData",
-                inputs, outputs);
-
-        CalDataResidualConfig calDataResidualConfig = new CalDataResidualConfig();
-
-        McmcCookingConfig mcmcCookingConfig = new McmcCookingConfig();
-        McmcSummaryConfig mcmcSummaryConfig = new McmcSummaryConfig();
-        // FIXME: a IMcmc should be an argument that provides the MCMC configuration
-        McmcConfig mcmcConfig = new McmcConfig();
-
-        CalibrationConfig fakeCalibrationConfig = new CalibrationConfig(
-                model,
-                modelOutputs,
-                calibrationData,
-                mcmcConfig,
-                mcmcCookingConfig,
-                mcmcSummaryConfig,
-                calDataResidualConfig);
-
-        RunOptions runOptions = new RunOptions(
-                true,
-                true,
-                true,
-                true);
-
-        PredictionConfig[] predConfigs = new PredictionConfig[priorPredictionExperiment.length];
-        for (int k = 0; k < priorPredictionExperiment.length; k++) {
-            predConfigs[k] = priorPredictionExperiment[k].getPredictionConfig();
-        }
-
-        bam = new BaM(
-                fakeCalibrationConfig,
-                predConfigs,
-                runOptions,
-                null,
-                null);
-
-        isConfigured = true;
-    }
-
-    // @Override
-    // public void run() {
-    // if (!isConfigured) {
-    // System.err.println("Cannot run BaM if configure() method has not been called
-    // first!");
-    // return;
-    // }
-    // try {
-    // bam.run(workspace.toString(), txt -> {
-    // System.out.println("log => " + txt);
-    // });
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-
-    // bam.readResults(workspace.toString());
-    // hasResults = true;
-
-    // try {
-    // backupBamRun();
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }
-
-    // @Override
-    // protected void backupBamRun() throws IOException {
-    // // 1/ creata temporary dir
-
-    // System.out.println("App.TEMP_DIR; ==> " + App.TEMP_DIR);
-    // File tD = new File(App.TEMP_DIR);
-    // boolean created = tD.mkdirs();
-    // System.out.println("Created ==> " + created);
-
-    // // 2/ create zip containing BaM run files
-
-    // File zipFile = new File(runZipFile.toString());
-    // FileOutputStream zipFileOutStream = new FileOutputStream(zipFile);
-    // ZipOutputStream zipOutStream = new ZipOutputStream(zipFileOutStream);
-
-    // File[] files = workspace.toFile().listFiles();
-    // if (files != null) {
-    // for (File f : files) {
-    // System.out.println("File '" + f + "'.");
-    // ZipEntry ze = new ZipEntry(f.getName());
-    // zipOutStream.putNextEntry(ze);
-    // Files.copy(f.toPath(), zipOutStream);
-    // }
-    // }
-    // zipOutStream.close();
-    // }
 
 }
