800b2a039da87ccb906eee833823ff931b0248f8
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-09-04 12:57:23 +0200

    refactored RadioButtons class

--- src/main/java/org/baratinage/ui/component/RadioButtons.java
+++ src/main/java/org/baratinage/ui/component/RadioButtons.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
