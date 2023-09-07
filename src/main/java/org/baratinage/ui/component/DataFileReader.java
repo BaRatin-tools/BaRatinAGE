22571ee3d405b513b6dc4d6ed741c615cc8d5356
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-09-07 09:00:25 +0200

    refactored data import code
    necessary to make it usable for both gaugings and limnigraphs (and hopefull other types of data)

---
+++ src/main/java/org/baratinage/ui/component/DataFileReader.java
@@ -0,0 +1,254 @@
+package org.baratinage.ui.component;
+
+import java.awt.Font;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JSeparator;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileFilter;
+
+import org.baratinage.ui.AppConfig;
+import org.baratinage.ui.container.GridPanel;
+import org.baratinage.ui.container.RowColPanel;
+import org.baratinage.ui.lg.Lg;
+import org.baratinage.utils.ReadFile;
+
+public class DataFileReader extends RowColPanel {
+    public String filePath = null;
+    public String sep = "\t";
+    public int nRowSkip = 0;
+    public String missingValueString = "";
+    public boolean hasHeaderRow = true;
+    public int nPreload = 15;
+
+    private JLabel selectedFilePathLabel;
+    private String[] fileLines;
+
+    public DataFileReader() {
+        super(AXIS.COL);
+
+        // **********************************************************
+        // file selection section
+
+        GridPanel importFilePanel = new GridPanel();
+        importFilePanel.setPadding(5);
+        importFilePanel.setGap(5);
+        importFilePanel.setColWeight(1, 1);
+        // importFilePanel.setRowWeight(2, 10000);
+        JLabel explainLabel = new JLabel();
+        Lg.register(explainLabel, "select_file_to_import");
+        selectedFilePathLabel = new JLabel();
+        selectedFilePathLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+        JButton browseFileSystemButon = new JButton();
+        Lg.register(browseFileSystemButon, "browse");
+        browseFileSystemButon.addActionListener(e -> {
+            final JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File(AppConfig.AC.lastUsedDir));
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser.setFileFilter(new FileFilter() {
+
+                @Override
+                public boolean accept(File f) {
+                    boolean dir = f.isDirectory();
+                    boolean txt = f.getName().endsWith(".txt");
+                    boolean csv = f.getName().endsWith(".csv");
+                    boolean bad = f.getName().endsWith(".bad");
+                    boolean dat = f.getName().endsWith(".dat");
+                    return dir || txt || csv || bad || dat;
+                }
+
+                @Override
+                public String getDescription() {
+                    return Lg.text("data_text_file") + " (.txt, .csv, .dat, .bad)";
+                }
+
+            });
+            fileChooser.setDialogTitle(Lg.text("select_data_text_file"));
+            fileChooser.setAcceptAllFileFilterUsed(false);
+            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                readFilesLines(fileChooser.getSelectedFile().getAbsolutePath());
+                fireChangeListeners();
+            }
+
+        });
+
+        importFilePanel.insertChild(explainLabel, 0, 0, 2, 1);
+        importFilePanel.insertChild(browseFileSystemButon, 0, 1);
+        importFilePanel.insertChild(selectedFilePathLabel, 1, 1);
+
+        // **********************************************************
+        // import settings section
+
+        GridPanel importSettingsPanel = new GridPanel();
+        importSettingsPanel.setColWeight(1, 1);
+        importSettingsPanel.setGap(5, 5);
+        importSettingsPanel.setPadding(5);
+
+        JLabel separatorLabel = new JLabel(Lg.text("separator"));
+
+        RadioButtons separatorOptions = new RadioButtons();
+        separatorOptions.addOption("\t", new JRadioButton(Lg.text("sep_tab")));
+        separatorOptions.addOption(";", new JRadioButton(Lg.text("sep_semicolon")));
+        separatorOptions.addOption(",", new JRadioButton(Lg.text("sep_comma")));
+        separatorOptions.addOption(" ", new JRadioButton(Lg.text("sep_space")));
+        separatorOptions.addChangeListener((chEvt) -> {
+            sep = separatorOptions.getSelectedValue();
+            fireChangeListeners();
+        });
+        separatorOptions.setSelectedValue("\t");
+
+        importSettingsPanel.insertChild(separatorLabel, 0, 0);
+        importSettingsPanel.insertChild(separatorOptions, 1, 0);
+
+        JCheckBox hasHeaderCheckBox = new JCheckBox(Lg.text("has_header_row"));
+        hasHeaderCheckBox.setSelected(hasHeaderRow);
+        hasHeaderCheckBox.addActionListener((e) -> {
+            hasHeaderRow = hasHeaderCheckBox.isSelected();
+            fireChangeListeners();
+        });
+
+        importSettingsPanel.insertChild(hasHeaderCheckBox, 0, 1, 2, 1);
+
+        JLabel nSkipRowLabel = new JLabel(Lg.text("n_rows_to_skip"));
+        SpinnerNumberModel spinnerModel = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
+        JSpinner nSkipRowField = new JSpinner(spinnerModel);
+        nSkipRowField.addChangeListener((e) -> {
+            nRowSkip = (int) nSkipRowField.getValue();
+            fireChangeListeners();
+        });
+
+        importSettingsPanel.insertChild(nSkipRowLabel, 0, 2);
+        importSettingsPanel.insertChild(nSkipRowField, 1, 2);
+
+        JLabel missingValueCodeLabel = new JLabel(Lg.text("missing_value_code"));
+        TextField missingValueCodeField = new TextField();
+        missingValueCodeField.setText(missingValueString);
+        missingValueCodeField.addChangeListener((txt) -> {
+            missingValueString = txt;
+            fireChangeListeners();
+        });
+
+        importSettingsPanel.insertChild(missingValueCodeLabel, 0, 3);
+        importSettingsPanel.insertChild(missingValueCodeField, 1, 3);
+
+        JLabel nRowPreloadLabel = new JLabel(Lg.text("n_rows_to_preload"));
+        SpinnerNumberModel nRowPreloadSpinnerModel = new SpinnerNumberModel(nPreload, 10, Integer.MAX_VALUE, 1);
+        JSpinner nRowPreloadField = new JSpinner(nRowPreloadSpinnerModel);
+        nRowPreloadField.addChangeListener((e) -> {
+            nPreload = (int) nRowPreloadField.getValue();
+            fireChangeListeners();
+        });
+
+        importSettingsPanel.insertChild(nRowPreloadLabel, 0, 4);
+        importSettingsPanel.insertChild(nRowPreloadField, 1, 4);
+
+        // **********************************************************
+        // final panel
+
+        appendChild(importFilePanel, 0);
+        appendChild(new JSeparator(), 0);
+        appendChild(importSettingsPanel, 0);
+
+    }
+
+    private void readFilesLines(String filePath) {
+
+        try {
+            fileLines = ReadFile.getLines(
+                    filePath,
+                    Integer.MAX_VALUE,
+                    true);
+        } catch (IOException e) {
+            System.err.println("ERROR while trying to read file!");
+            return;
+        }
+        this.filePath = filePath;
+        String displayPath = filePath;
+        try {
+            displayPath = (new File(filePath)).getCanonicalPath();
+        } catch (IOException e) {
+        }
+        selectedFilePathLabel.setText(displayPath);
+        selectedFilePathLabel.setToolTipText(displayPath);
+    }
+
+    public List<String[]> getData(int nRows) {
+
+        if (fileLines == null || fileLines.length <= nRowSkip) {
+            List<String[]> dataString = new ArrayList<>();
+            dataString.add(new String[0]);
+            return dataString;
+        }
+
+        List<String[]> dataString = ReadFile.linesToStringMatrix(
+                fileLines,
+                sep,
+                nRowSkip + (hasHeaderRow ? 1 : 0),
+                nRows,
+                true);
+
+        return dataString;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public List<String[]> getData() {
+        return getData(Integer.MAX_VALUE);
+    }
+
+    public String[] getHeaders() {
+
+        if (fileLines == null || fileLines.length <= nRowSkip) {
+            return new String[0];
+        }
+
+        List<String[]> dataString = ReadFile.linesToStringMatrix(
+                fileLines,
+                sep,
+                nRowSkip,
+                1,
+                true);
+
+        int nCol = dataString.size();
+        String[] headers = new String[nCol];
+        if (hasHeaderRow) {
+            headers = ReadFile.getStringRow(dataString, 0);
+        } else {
+            for (int k = 0; k < nCol; k++) {
+                headers[k] = "col #" + (k + 1);
+            }
+        }
+
+        return headers;
+    }
+
+    private final List<ChangeListener> changeListeners = new ArrayList<>();
+
+    public void addChangeListener(ChangeListener l) {
+        changeListeners.add(l);
+    }
+
+    public void removeChangeListener(ChangeListener l) {
+        changeListeners.remove(l);
+    }
+
+    public void fireChangeListeners() {
+        for (ChangeListener l : changeListeners) {
+            l.stateChanged(new ChangeEvent(this));
+        }
+    }
+
+}
---
+++ src/main/java/org/baratinage/ui/component/DataParser.java
@@ -0,0 +1,251 @@
+package org.baratinage.ui.component;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import javax.swing.BorderFactory;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+
+import org.baratinage.ui.AppConfig;
+import org.baratinage.ui.container.RowColPanel;
+
+public class DataParser extends RowColPanel {
+
+    private enum COL_TYPE {
+        DOUBLE, DATETIME
+    }
+
+    private record ColSettings(
+            COL_TYPE type,
+            String dateTimeFormat) {
+    };
+
+    private String missingValueCode;
+    // private String[] headers;
+    private List<String[]> rawData;
+
+    private DataTableModel dataTableModel;
+    private JTable dataTable;
+    private Map<Integer, ColSettings> colSettings = new HashMap<>();
+
+    private static final Function<String, Double> TO_DOUBLE = (String v) -> {
+        try {
+            Double d = Double.parseDouble(v);
+            return d == null ? Double.NaN : d;
+        } catch (Exception e) {
+            return Double.NaN;
+        }
+    };
+
+    private static final Predicate<String> DOUBLE_VALIDATOR = (String v) -> {
+        try {
+            Double d = Double.parseDouble(v);
+            return !(d == null || d.isNaN());
+        } catch (Exception e) {
+            return false;
+        }
+    };
+
+    private static final CustomCellRenderer IGNORE_COL_RENDERER = new CustomCellRenderer();
+
+    public DataParser() {
+
+        setPadding(5);
+        dataTableModel = new DataTableModel();
+
+        dataTable = new JTable(dataTableModel);
+
+        dataTable.getTableHeader().setReorderingAllowed(false);
+        dataTable.setCellSelectionEnabled(true);
+
+        JScrollPane scrollpane = new JScrollPane(dataTable);
+        scrollpane.setBorder(BorderFactory.createEmptyBorder());
+
+        Dimension defaultPrefDim = scrollpane.getPreferredSize();
+        defaultPrefDim.height = 300;
+        scrollpane.setPreferredSize(defaultPrefDim);
+        appendChild(scrollpane);
+
+    }
+
+    public void setRawData(List<String[]> rawData, String[] headers, String missingValueCode) {
+
+        this.rawData = rawData;
+        this.missingValueCode = missingValueCode;
+
+        dataTableModel.setRawData(rawData);
+
+        int nCol = rawData.size();
+        JTableHeader tableHeader = dataTable.getTableHeader();
+        TableColumnModel tableColModel = tableHeader.getColumnModel();
+
+        CustomCellRenderer doubleColRenderer = new CustomCellRenderer(missingValueCode, DOUBLE_VALIDATOR);
+
+        for (int k = 0; k < nCol; k++) {
+            TableColumn tableCol = tableColModel.getColumn(k);
+            ColSettings cs = colSettings.get(k);
+            if (cs == null) {
+                tableCol.setCellRenderer(IGNORE_COL_RENDERER);
+            } else if (cs.type == COL_TYPE.DOUBLE) {
+                tableCol.setCellRenderer(doubleColRenderer);
+            } else if (cs.type == COL_TYPE.DATETIME) {
+                Predicate<String> dataTimeValidator = (String v) -> {
+                    try {
+                        LocalDateTime.parse(v, DateTimeFormatter.ofPattern(cs.dateTimeFormat));
+                        return true;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                };
+                tableCol.setCellRenderer(new CustomCellRenderer(missingValueCode, dataTimeValidator));
+            }
+            if (headers != null && headers.length > k) {
+                tableCol.setHeaderValue(headers[k]);
+            }
+        }
+    }
+
+    public void ignoreAll() {
+        colSettings.clear();
+    }
+
+    public void ignoreCol(int colIndex) {
+        colSettings.remove(colIndex);
+    }
+
+    public void setAsDoubleCol(int colIndex) {
+        colSettings.put(colIndex, new ColSettings(COL_TYPE.DOUBLE, ""));
+    }
+
+    public void setAsDateTimeCol(int colIndex, String format) {
+        colSettings.put(colIndex, new ColSettings(COL_TYPE.DATETIME, format));
+    }
+
+    public double[] getDoubleCol(int colIndex) {
+        Function<String, Double> converter = (String v) -> {
+            if (v.equals(missingValueCode)) {
+                return Double.NaN;
+            } else {
+                return TO_DOUBLE.apply(v);
+            }
+        };
+        String[] rawCol = rawData.get(colIndex);
+        int n = rawCol.length;
+        double[] col = new double[n];
+        for (int k = 0; k < n; k++) {
+            col[k] = converter.apply(rawCol[k]);
+        }
+        return col;
+    }
+
+    private static class CustomCellRenderer extends DefaultTableCellRenderer {
+
+        private boolean ignored;
+        private Predicate<String> validator;
+        private String missingValueCode;
+
+        public CustomCellRenderer() {
+            ignored = true;
+        }
+
+        public CustomCellRenderer(String missingValueCode, Predicate<String> validator) {
+            this.validator = validator;
+            this.missingValueCode = missingValueCode;
+            ignored = false;
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(
+                JTable table, Object value,
+                boolean isSelected, boolean hasFocus,
+                int row, int column) {
+
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            String valStr = value.toString();
+
+            if (ignored) {
+                setForeground(Color.LIGHT_GRAY);
+                // setFont(getFont().deriveFont(Font.ITALIC));
+                return this;
+            }
+
+            if (valStr.equals(missingValueCode)) {
+                setForeground(AppConfig.AC.INVALID_COLOR);
+                setFont(getFont().deriveFont(Font.PLAIN).deriveFont(Font.ITALIC));
+            } else {
+                setFont(getFont().deriveFont(Font.BOLD));
+                if (!validator.test(valStr)) {
+                    setForeground(AppConfig.AC.INVALID_COLOR);
+                    setFont(getFont().deriveFont(Font.ITALIC).deriveFont(Font.BOLD));
+                } else {
+                    setForeground(isSelected ? table.getSelectionForeground() : table.getForeground());
+                    // setFont(getFont().deriveFont(Font.PLAIN).deriveFont(Font.BOLD));
+                }
+            }
+            return this;
+        }
+    }
+
+    private class DataTableModel extends AbstractTableModel {
+
+        private List<String[]> rawData = new ArrayList<>();
+
+        public boolean setRawData(List<String[]> rawData) {
+            int nCol = rawData.size();
+            if (nCol == 0) {
+                this.rawData = new ArrayList<>();
+                return true;
+            }
+            int nRow = rawData.get(0).length;
+            if (nRow == 0) {
+                this.rawData = new ArrayList<>();
+                return true;
+            }
+            for (String[] col : rawData) {
+                if (col.length != nRow) {
+                    System.err.println("Inconsistent number of rows... Aborting.");
+                    return false;
+                }
+            }
+            this.rawData = rawData;
+            fireTableStructureChanged();
+            fireTableDataChanged();
+            return true;
+        }
+
+        @Override
+        public int getRowCount() {
+            return getColumnCount() > 0 ? rawData.get(0).length : 0;
+
+        }
+
+        @Override
+        public int getColumnCount() {
+            return rawData.size();
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return rawData.get(columnIndex)[rowIndex];
+        }
+
+    }
+
+}
