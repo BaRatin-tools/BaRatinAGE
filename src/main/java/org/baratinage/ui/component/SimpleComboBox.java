0cf827c3c552287dd30d633a211149477d3b95a0
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-09-12 10:08:05 +0200

    Update SimpleComboBox.java
    added validators (similar to SimpleTextField and NumberField)
    added a silent option when setting items or selected current item

--- src/main/java/org/baratinage/ui/component/SimpleComboBox.java
+++ src/main/java/org/baratinage/ui/component/SimpleComboBox.java
@@ -1,11 +1,14 @@
 package org.baratinage.ui.component;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import javax.swing.JComboBox;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicBorders;
 import javax.swing.DefaultComboBoxModel;
 
 import org.baratinage.ui.container.RowColPanel;
@@ -14,13 +17,17 @@ public class SimpleComboBox extends RowColPanel {
 
     private DefaultComboBoxModel<String> model;
     private JComboBox<String> comboBox;
+    private boolean changeListenersDisabled = false;
 
     public SimpleComboBox() {
         model = new DefaultComboBoxModel<>();
         comboBox = new JComboBox<>();
         comboBox.setModel(model);
         comboBox.addActionListener((e) -> {
-            fireChangeListeners();
+            if (!changeListenersDisabled) {
+                fireChangeListeners();
+            }
+            fireValidators();
         });
         appendChild(comboBox);
     }
@@ -31,11 +38,19 @@ public class SimpleComboBox extends RowColPanel {
     }
 
     public void setItems(String[] items) {
+        setItems(items, false);
+    }
+
+    public void setItems(String[] items, boolean silent) {
+        if (silent) {
+            changeListenersDisabled = true;
+        }
         model.removeAllElements();
         model.addElement("-"); // empty element
         for (String item : items) {
             model.addElement(item);
         }
+        changeListenersDisabled = false;
     }
 
     public void clearItems() {
@@ -43,8 +58,16 @@ public class SimpleComboBox extends RowColPanel {
     }
 
     public void setSelectedItem(int k) {
+        setSelectedItem(k, false);
+    }
+
+    public void setSelectedItem(int k, boolean silent) {
+        if (silent) {
+            changeListenersDisabled = true;
+        }
         if (k < 0) {
             model.setSelectedItem(model.getElementAt(0));
+            changeListenersDisabled = false;
             return;
         }
         k += 1;
@@ -52,6 +75,7 @@ public class SimpleComboBox extends RowColPanel {
         if (label != null) {
             model.setSelectedItem(label);
         }
+        changeListenersDisabled = false;
     }
 
     public int getSelectedIndex() {
@@ -67,10 +91,38 @@ public class SimpleComboBox extends RowColPanel {
         return model.getSize() - 1;
     }
 
+    private final List<Predicate<Integer>> validators = new ArrayList<>();
+
+    public void addValidator(Predicate<Integer> validator) {
+        this.validators.add(validator);
+    }
+
+    public void removeValidator(Predicate<Integer> validator) {
+        this.validators.remove(validator);
+    }
+
+    public void fireValidators() {
+        setValidity(true);
+        for (Predicate<Integer> p : validators) {
+            if (!p.test(getSelectedIndex())) {
+                setValidity(false);
+            }
+        }
+    }
+
+    private void setValidity(boolean isValid) {
+        Color color = new Color(125, 255, 125, 0);
+        if (!isValid) {
+            color = new Color(255, 125, 125, 200);
+        }
+        setBorder(new BasicBorders.FieldBorder(color, color, color, color));
+    }
+
     private final List<ChangeListener> changeListeners = new ArrayList<>();
 
     public void addChangeListener(ChangeListener l) {
         changeListeners.add(l);
+        System.out.println();
     }
 
     public void removeChangeListener(ChangeListener l) {
