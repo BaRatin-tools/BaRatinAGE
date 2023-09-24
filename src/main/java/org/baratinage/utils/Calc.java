4ed06ba050a1b17a6cd61379c164a655009a8fae
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-09-24 19:48:33 +0200

    added smooth method

--- src/main/java/org/baratinage/utils/Calc.java
+++ src/main/java/org/baratinage/utils/Calc.java
@@ -38,7 +38,17 @@ public class Calc {
     }
 
     public static double[] range(double[] values) {
-        return new double[] { Calc.min(values), Calc.max(values) };
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+        for (double d : values) {
+            if (d < min) {
+                min = d;
+            }
+            if (d > max) {
+                max = d;
+            }
+        }
+        return new double[] { min, max };
     }
 
     public static double[] copy(double[] values) {
@@ -50,10 +60,18 @@ public class Calc {
         return copy;
     }
 
-    public static double[] percentiles(double[] values, double probabilities[]) {
+    public static double[] sort(double[] values) {
+        double[] sorted = copy(values);
+        Arrays.sort(sorted);
+        return sorted;
+    }
 
-        double[] copy = copy(values);
-        Arrays.sort(copy);
+    public static double[] percentiles(double[] values, double... probabilities) {
+        return percentiles(false, values, probabilities);
+    }
+
+    public static double[] percentiles(boolean valuesAlreadySorted, double[] values, double... probabilities) {
+        double[] sorted = valuesAlreadySorted ? values : sort(values);
         int n = values.length;
         int m = probabilities.length;
         double[] percentiles = new double[m];
@@ -63,9 +81,49 @@ public class Calc {
                 p = 1;
             if (p < 0)
                 p = 0;
-            int index = (int) Math.floor(p * n);
-            percentiles[k] = copy[index];
+            // int index = (int) Math.floor(p * n);
+            int index = Math.round((float) (p * n));
+            if (index >= n) {
+                System.out.println("ERROR!");
+                index = n - 1;
+            }
+            percentiles[k] = sorted[index];
         }
         return percentiles;
     }
+
+    public static boolean isEven(int value) {
+        // source: https://stackoverflow.com/a/7342273
+        return (value & 1) == 0;
+    }
+
+    public static double[] smooth(double[] toSmooth, int windowSize) {
+        System.out.println(windowSize);
+        if (windowSize < 2) {
+            return toSmooth;
+        }
+        if (isEven(windowSize)) {
+            System.out.println("Even window size unsupported! Increasing window size by one!");
+            windowSize++;
+        }
+        int n = toSmooth.length;
+        double[] smoothed = new double[n];
+        int halfWindowSize = (int) (windowSize / 2d);
+        for (int k = 0; k < n; k++) {
+            if (k >= halfWindowSize && k < n - halfWindowSize) {
+                double s = 0;
+                for (int i = 0; i < windowSize; i++) {
+                    int index = k - halfWindowSize + i;
+                    s += toSmooth[index];
+                }
+                smoothed[k] = s / windowSize;
+            } else {
+                smoothed[k] = Double.NaN;
+            }
+
+        }
+        return smoothed;
+
+    }
+
 }
