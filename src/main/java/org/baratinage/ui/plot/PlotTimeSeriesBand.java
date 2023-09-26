7d2444b34f1beabbc88badf387133470ae9d943e
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-09-26 22:32:11 +0200

    added alpha as constructor parameter

--- src/main/java/org/baratinage/ui/plot/PlotTimeSeriesBand.java
+++ src/main/java/org/baratinage/ui/plot/PlotTimeSeriesBand.java
@@ -10,14 +10,14 @@ public class PlotTimeSeriesBand extends PlotBand {
     public PlotTimeSeriesBand(String label,
             Second[] time, double[] ylow, double[] yhigh,
             Paint paint) {
-        this(label, time, ylow, ylow, yhigh, paint, paint, buildEmptyStroke());
+        this(label, time, ylow, ylow, yhigh, paint, 0.9f, paint, buildEmptyStroke());
     }
 
     public PlotTimeSeriesBand(String label,
             Second[] time, double[] y, double[] ylow, double[] yhigh,
-            Paint fillPaint,
+            Paint fillPaint, float alpha,
             Paint linePaint, Stroke lineStroke) {
-        super(label, secondToDouble(time), y, ylow, yhigh, fillPaint, linePaint, lineStroke);
+        super(label, secondToDouble(time), y, ylow, yhigh, fillPaint, alpha, linePaint, lineStroke);
     }
 
 }
