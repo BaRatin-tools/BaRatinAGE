13d622e439f860503108d931de94f70cd9e7b1be
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-10-02 11:12:42 +0200

    modified placeholder location so it is always visible

--- src/main/java/org/baratinage/ui/component/SimpleTextField.java
+++ src/main/java/org/baratinage/ui/component/SimpleTextField.java
@@ -1,6 +1,8 @@
 package org.baratinage.ui.component;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -9,6 +11,9 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
@@ -18,16 +23,32 @@ public class SimpleTextField extends JTextField {
 
     private String placeholder;
 
+    private boolean placeholderAlwaysVisibleOnTop = true;
+    private final Border defaultBorder;
+    private final Font placeholderFont;
+    private final Color placeholderColor;
+
     public SimpleTextField() {
         super();
 
+        int H = 35;
+
         Dimension prefDim = this.getPreferredSize();
         prefDim.width = 100;
+        prefDim.height = H;
         Dimension minDim = getMinimumSize();
         minDim.width = 50;
+        minDim.height = H;
+        Dimension maxDim = getMaximumSize();
+        maxDim.height = H;
 
         setPreferredSize(prefDim);
         setMinimumSize(minDim);
+        setMinimumSize(maxDim);
+
+        defaultBorder = getBorder();
+        placeholderFont = getFont().deriveFont(13f);
+        placeholderColor = new Color(150, 150, 175);
 
         getDocument().addDocumentListener(new DocumentListener() {
 
@@ -52,9 +73,21 @@ public class SimpleTextField extends JTextField {
     // Placeholder implementation comes from: https://stackoverflow.com/a/16229082
     @Override
     protected void paintComponent(final Graphics g) {
+
+        boolean hasNoPlaceHolder = placeholder == null || placeholder.length() == 0;
+
+        if (placeholderAlwaysVisibleOnTop && !hasNoPlaceHolder) {
+            setBorder(new CompoundBorder(defaultBorder, new EmptyBorder(12, 0, 0, 0)));
+        } else {
+            setBorder(defaultBorder);
+        }
+
         super.paintComponent(g);
 
-        if (placeholder == null || placeholder.length() == 0 || getText().length() > 0) {
+        if (hasNoPlaceHolder) {
+            return;
+        }
+        if (!placeholderAlwaysVisibleOnTop && getText().length() > 0) {
             return;
         }
 
@@ -62,15 +95,19 @@ public class SimpleTextField extends JTextField {
         g2D.setRenderingHint(
                 RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        g2D.setColor(getDisabledTextColor());
+        g2D.setColor(placeholderColor);
+        g2D.setFont(placeholderFont);
         float maxCharHeight = g2D.getFontMetrics().getMaxAscent();
         float totalHeight = getHeight();
         float x = getInsets().left;
-
-        float y = maxCharHeight / 2 + totalHeight / 2 - getInsets().top / 2; // fixed vertical centering of placeholder
-                                                                             // text
-        // float y = maxCharHeight + getInsets().top; // original value
-        // x, y is the baseline of first character
+        float y;
+        // original value x, y is the baseline of first character
+        if (!placeholderAlwaysVisibleOnTop) {
+            // fixed vertical centering of placeholder
+            y = maxCharHeight / 2 + totalHeight / 2 - getInsets().top / 2;
+        } else {
+            y = maxCharHeight;
+        }
         g2D.drawString(placeholder, x, y);
     }
 
