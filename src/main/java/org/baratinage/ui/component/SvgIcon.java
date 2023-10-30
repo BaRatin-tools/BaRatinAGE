bab405c4aef1ee9510218286e174d4db32c51191
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-10-30 11:50:14 +0100

    refactored SvgIcon and removed NoScalingIcon

--- src/main/java/org/baratinage/ui/component/SvgIcon.java
+++ src/main/java/org/baratinage/ui/component/SvgIcon.java
@@ -1,19 +1,17 @@
 package org.baratinage.ui.component;
 
 import java.awt.Component;
-import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 
 import javax.swing.ImageIcon;
-
 import org.apache.batik.transcoder.TranscoderException;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
@@ -21,62 +19,132 @@ import org.apache.batik.transcoder.image.ImageTranscoder;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.baratinage.ui.AppConfig;
 
-public class SvgIcon extends NoScalingIcon {
+public class SvgIcon extends ImageIcon {
 
-    static public ImageIcon buildCustomAppImageIcon(String name) {
-        return buildCustomAppImageIcon(name, AppConfig.AC.ICON_SIZE);
-    }
+    private final float width;
+    private final float height;
 
-    private static ImageIcon buildCustomAppImageIcon(String name, float size) {
-        return new SvgIcon(Path.of(
-                AppConfig.AC.ICONS_RESOURCES_DIR,
-                "custom", name).toString(), size, size);
-    }
-
-    static public ImageIcon buildFeatherAppImageIcon(String name) {
-        return buildFeatherAppImageIcon(name, AppConfig.AC.ICON_SIZE * 0.75f);
-    }
-
-    private static ImageIcon buildFeatherAppImageIcon(String name, float size) {
-        return new SvgIcon(Path.of(
-                AppConfig.AC.ICONS_RESOURCES_DIR,
-                "feather", name).toString(), size, size);
-    }
+    private final byte[] svgBytes;
+    private final String sourcePath;
 
     public SvgIcon(String path, float width, float height) {
         super();
-        // source: https://stackoverflow.com/a/20664243
+
+        this.sourcePath = path;
+        this.width = width;
+        this.height = height;
+
+        byte[] b = new byte[0];
         try {
-
-            Frame[] frames = Frame.getFrames();
-            Component component = frames[0].getComponent(0);
-
-            Graphics g = component.getGraphics();
-            if (g != null) {
-                Graphics2D g2d = (Graphics2D) g.create();
-                AffineTransform aT = g2d.getTransform();
-                g2d.dispose();
-                double scaleX = aT.getScaleX();
-                double scaleY = aT.getScaleY();
-                width = width * (float) scaleX;
-                height = height * (float) scaleY;
-            }
-
-            BufferedImageTranscoder imgTranscoder = new BufferedImageTranscoder();
-            imgTranscoder.addTranscodingHint(PNGTranscoder.KEY_WIDTH, width);
-            imgTranscoder.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, height);
-            InputStream inputStream = new FileInputStream(new File(path));
-            TranscoderInput transcoderInput = new TranscoderInput(inputStream);
-            imgTranscoder.transcode(transcoderInput, null);
-            BufferedImage bSrc = imgTranscoder.getBufferedImage();
-            setImage(bSrc);
-        } catch (TranscoderException | FileNotFoundException e) {
+            InputStream inputStream = new FileInputStream(new File(sourcePath));
+            b = inputStream.readAllBytes();
+        } catch (IOException e) {
+            System.err.println("SvgIcon Error: Error while retrieving SVG file");
             e.printStackTrace();
         }
 
+        this.svgBytes = b;
+
+        buildIcon();
     };
 
-    private class BufferedImageTranscoder extends ImageTranscoder {
+    public void rebuildIcon() {
+        buildIcon();
+    }
+
+    private void buildIcon() {
+
+        float w = width;
+        float h = height;
+
+        Scales s = getScales();
+
+        w = width * (float) s.x;
+        h = height * (float) s.y;
+
+        BufferedImageTranscoder imgTranscoder = new BufferedImageTranscoder();
+        imgTranscoder.addTranscodingHint(PNGTranscoder.KEY_WIDTH, w);
+        imgTranscoder.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, h);
+
+        try {
+            InputStream inputStream = new ByteArrayInputStream(svgBytes);
+            TranscoderInput transcoderInput = new TranscoderInput(inputStream);
+            imgTranscoder.transcode(transcoderInput, null);
+        } catch (TranscoderException e) {
+            System.err.println("SvgIcon Error: error while build raster image from SVG.");
+            e.printStackTrace();
+            return;
+        }
+        BufferedImage image = imgTranscoder.getBufferedImage();
+        setImage(image);
+    }
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+
+        // See: https://stackoverflow.com/a/65742492
+
+        Graphics2D g2d = (Graphics2D) g.create();
+        AffineTransform aT = g2d.getTransform();
+
+        double sX = aT.getScaleX();
+        double sY = aT.getScaleY();
+
+        g2d.scale(1 / sX, 1 / sY);
+
+        int iconHeight = super.getIconHeight();
+        int componentHeight = c.getHeight();
+
+        y = scale(componentHeight, sY, false) / 2 - iconHeight / 2;
+
+        super.paintIcon(c, g2d, x, y);
+        g2d.dispose();
+
+    }
+
+    @Override
+    public int getIconWidth() {
+        int d = super.getIconWidth();
+        d = scale(d, getScales().x, true);
+        return d;
+    }
+
+    @Override
+    public int getIconHeight() {
+        int d = super.getIconHeight();
+        d = scale(d, getScales().y, true);
+        return d;
+    }
+
+    private static record Scales(double x, double y) {
+        @Override
+        public String toString() {
+            return "" + x + "," + y;
+        }
+    };
+
+    private static int scale(int value, double factor, boolean inverse) {
+        if (inverse) {
+            return (int) Math.round(value / factor);
+        } else {
+            return (int) Math.round(value * factor);
+        }
+    }
+
+    private static Scales getScales() {
+        Graphics g = AppConfig.AC.APP_MAIN_FRAME.getGraphics();
+        if (g == null) {
+            return new Scales(1, 1);
+        }
+        Graphics2D g2d = (Graphics2D) g.create();
+        AffineTransform aT = g2d.getTransform();
+        g2d.dispose();
+        double sX = aT.getScaleX();
+        double sY = aT.getScaleY();
+        return new Scales(sX, sY);
+    }
+
+    private static class BufferedImageTranscoder extends ImageTranscoder {
 
         private BufferedImage bufferedImage = null;
 
