29cd1fea2c7dd10f0a9665cffab65b26a04302e7
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-10-31 11:48:30 +0100

    added method to build LocalDateTime array from  years, months, etc. double[] arrays

--- src/main/java/org/baratinage/utils/DateTime.java
+++ src/main/java/org/baratinage/utils/DateTime.java
@@ -21,4 +21,41 @@ public class DateTime {
         }
         return dateTime;
     }
+
+    public static LocalDateTime[] ymdhmsDoubleToTimeVector(double[] years, double[] months, double[] days,
+            double[] hours, double[] minutes, double[] seconds) {
+        int n = years.length;
+        if (months.length != n) {
+            System.err.println("DateTime Error: 'months' vector length not matching 'years' vector length!");
+            return null;
+        }
+        if (days.length != n) {
+            System.err.println("DateTime Error: 'days' vector length not matching 'years' vector length!");
+            return null;
+        }
+        if (hours.length != n) {
+            System.err.println("DateTime Error: 'hours' vector length not matching 'years' vector length!");
+            return null;
+        }
+        if (minutes.length != n) {
+            System.err.println("DateTime Error: 'minutes' vector length not matching 'years' vector length!");
+            return null;
+        }
+        if (seconds.length != n) {
+            System.err.println("DateTime Error: 'seconds' vector length not matching 'years' vector length!");
+            return null;
+        }
+        LocalDateTime[] dateTime = new LocalDateTime[n];
+        for (int k = 0; k < n; k++) {
+            dateTime[k] = LocalDateTime.of(
+                    (int) years[k],
+                    (int) months[k],
+                    (int) days[k],
+                    (int) hours[k],
+                    (int) minutes[k],
+                    (int) seconds[k],
+                    0);
+        }
+        return dateTime;
+    }
 }
