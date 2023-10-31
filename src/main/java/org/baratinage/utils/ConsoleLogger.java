0bdd6a174342a294aa12fbbb3914941372a2c754
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2023-10-31 13:35:14 +0100

    added optional show/hide filters to ConsoleLogger

--- src/main/java/org/baratinage/utils/ConsoleLogger.java
+++ src/main/java/org/baratinage/utils/ConsoleLogger.java
@@ -7,7 +7,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ConsoleLogger {
 
@@ -54,51 +56,105 @@ public class ConsoleLogger {
         }
     }
 
-    private static String getCallerInfo() {
+    private static String getCallerClassName() {
         StackTraceElement[] trace = Thread.currentThread().getStackTrace();
-        if (trace.length > 4) {
-            String[] className = trace[4].getClassName().split("\\.");
-            // String methodName = trace[4].getMethodName();
-            return className[className.length - 1];
+        if (trace.length < 1) {
+            return "";
+        }
+        for (int k = 1; k < trace.length; k++) {
+            String[] fullClassName = trace[k].getClassName().split("\\.");
+            String className = fullClassName[fullClassName.length - 1];
+            if (!className.equals("ConsoleLogger")) {
+                return className;
+            }
         }
         return "";
     }
 
-    private static String parseMessage(String prefix, String message) {
-        return Misc.getTimeStamp("yyyy-MM-dd HH:mm:ss") + " : (" + getCallerInfo() + ")"
+    private static String parseMessage(String callerClassName, String prefix, String message) {
+        return Misc.getTimeStamp("yyyy-MM-dd HH:mm:ss") + " : (" + callerClassName + ")"
                 + (prefix.length() == 0 ? " " : " ***" + prefix + "*** ") + message;
     }
 
     public static void log(String message) {
-        String parsedMessage = parseMessage("", message);
-        System.out.println(parsedMessage);
+        String callerClassName = getCallerClassName();
+        String parsedMessage = parseMessage(callerClassName, "", message);
+        out(callerClassName, parsedMessage);
         addLogToLogFile(parsedMessage);
     }
 
     public static void error(String error) {
-        String parsedMessage = parseMessage("ERROR", error);
-        System.out.println(parsedMessage);
+        String callerClassName = getCallerClassName();
+        String parsedMessage = parseMessage(callerClassName, "ERROR", error);
+        System.err.println(parsedMessage);
         addLogToLogFile(parsedMessage);
     }
 
     public static void error(Exception exception) {
-        String parsedMessage = parseMessage("ERROR", "");
-        System.out.println(parsedMessage);
-        System.out.println(exception);
+        String callerClassName = getCallerClassName();
+        String parsedMessage = parseMessage(callerClassName, "ERROR", "");
+        System.err.println(parsedMessage);
+        System.err.println(exception);
         addLogToLogFile(parsedMessage + exception.toString());
     }
 
     public static void stackTrace(Exception exception) {
-        String parsedMessage = parseMessage("ERROR", "");
-        System.out.println(parsedMessage);
+        String callerClassName = getCallerClassName();
+        String parsedMessage = parseMessage(callerClassName, "ERROR", "");
+        out(callerClassName, parsedMessage);
         exception.printStackTrace();
         addLogToLogFile(parsedMessage + exception.toString());
     }
 
     public static void debuggingStackTrace() {
-        String parsedMessage = parseMessage("DEBUG", "");
-        System.out.println(parsedMessage);
+        String callerClassName = getCallerClassName();
+        String parsedMessage = parseMessage(callerClassName, "DEBUG", "");
+        out(callerClassName, parsedMessage);
         Thread.dumpStack();
+    }
+
+    private static List<String> showFilters = new ArrayList<>();
+
+    public static void addShowFilter(String filter) {
+        showFilters.add(filter);
+    }
+
+    public static void clearShowFilters() {
+        showFilters.clear();
+    }
+
+    private static List<String> hideFilters = new ArrayList<>();
+
+    public static void addHideFilters(String filter) {
+        hideFilters.add(filter);
+    }
+
+    public static void clearHideFilters() {
+        hideFilters.clear();
+    }
+
+    private static void out(String className, String message) {
+        if (showFilters.size() > 0) {
+            for (String f : showFilters) {
+                if (className.equals(f)) {
+                    System.out.println(message);
+                }
+            }
+        } else if (hideFilters.size() > 0) {
+            boolean show = true;
+            for (String f : hideFilters) {
+                if (className.equals(f)) {
+                    show = false;
+                    break;
+                }
+            }
+            if (show) {
+                System.out.println(message);
+            }
+        } else {
+            System.out.println(message);
+        }
+
     }
 
 }
