bbad60191fce8e44a0d5a000fe498cfe3d810b9c
IvanHeriver <ihach_ihach@hotmail.com>
2023-11-15 19:56:55 +0100

    made build script powershell only and working on linux

--- build-target-packaged.ps1
+++ build-target-packaged.ps1
@@ -9,15 +9,15 @@ $CONSOLE = $True
 $ICON_PATH = "resources/icons/icon.ico"
 
 $DIRS_TO_CREATE = @(
-   "log", "exe/bam_workspace"
+    "log", "exe/bam_workspace"
 )
 
 $DIRS_TO_COPY = @(
-   "resources", "example"
+    "resources", "example"
 )
 
 $FILES_TO_COPY = @(
-    "exe/BaM.exe", "exe/distribution.exe"
+    "exe/BaM", "exe/distribution"
 )
 
 
@@ -30,20 +30,24 @@ $NAME_VERSION = "$($NAME)-$($VERSION)"
 $TARGER_DIR = "target"
 $TARGET_PACKAGE_DIR = "target-packaged"
 $TARGET_PACKAGE_DIR_FULL = "$($TARGET_PACKAGE_DIR)/$($NAME_VERSION)"
-
+$RESOURCES_DIR =  $TARGET_PACKAGE_DIR_FULL
+if ($IsLinux) {
+    $RESOURCES_DIR = "$($TARGET_PACKAGE_DIR_FULL)/bin"
+}
 
 # cleaning up targer dir
-"Deleting content of build target directory..."
-rm "$($TARGER_DIR)/*" -R -FORCE
+if (Test-Path "$($TARGER_DIR)"  -PathType Container) {
+    "Removing folder '$($TARGER_DIR)'..."
+    Remove-Item -Path "$($TARGER_DIR)" -Recurse -Force
+}
 
 # preparing package app dir
-if (-Not (Test-Path $TARGET_PACKAGE_DIR -PathType Container)) {
-    "Creating packaged app target directory..."
-    mkdir $TARGET_PACKAGE_DIR | Out-Null
+if (-Not(Test-Path $TARGET_PACKAGE_DIR -PathType Container)) {
+    New-Item -ItemType "directory" -Path "$($TARGET_PACKAGE_DIR)" | Out-Null
 }
-if ((Test-Path $TARGET_PACKAGE_DIR_FULL -PathType Container)) {
-    "Creating packaged app target directory..."
-    rm $TARGET_PACKAGE_DIR_FULL -R -FORCE
+if (Test-Path $TARGET_PACKAGE_DIR_FULL -PathType Container) {
+    "Removing existing packaged app target directory '$($TARGET_PACKAGE_DIR_FULL)'..."
+    Remove-Item -Path "$($TARGET_PACKAGE_DIR_FULL)" -Recurse  -Force
 }
 
 # updating pom.xml version
@@ -66,31 +70,31 @@ $JPACKAGE_CMD = "$($JPACKAGE_CMD) --input $($TARGER_DIR)"
 $JPACKAGE_CMD = "$($JPACKAGE_CMD) --main-jar $($NAME_VERSION).jar"
 $JPACKAGE_CMD = "$($JPACKAGE_CMD) --icon $($ICON_PATH)"
     
-if ($CONSOLE) {
+if ($CONSOLE -And $IsWindows) {
     $JPACKAGE_CMD = "$($JPACKAGE_CMD) --win-console"
 } 
 
 $JPACKAGE_CMD 
 
-iex $JPACKAGE_CMD 
+Invoke-Expression $JPACKAGE_CMD 
 
 # creating necessary forlder
-foreach ( $DIR in $DIRS_TO_CREATE )
-{
-    "Creating folder '$($DIR)'..."
-    mkdir "$($TARGET_PACKAGE_DIR_FULL)/$($DIR)" | Out-Null
+foreach ( $DIR in $DIRS_TO_CREATE ) {
+    if (-Not (Test-Path "$($RESOURCES_DIR)/$($DIR)" -PathType Container)) {
+        "Creating folder '$($DIR)'..."
+        New-Item -ItemType "directory" -Path "$($RESOURCES_DIR)/$($DIR)" | Out-Null
+    }
 }
 
 # copying necessary folder
 foreach ( $DIR in $DIRS_TO_COPY )
 {
     "Copying folder '$($DIR)'..."
-    Copy-item -Force -Recurse  $DIR -Destination "$($TARGET_PACKAGE_DIR_FULL)/$($DIR)"
+    Copy-item -Force -Recurse  $DIR -Destination "$($RESOURCES_DIR)/$($DIR)"
 }
 
 # copying necessary files
-foreach ( $FILE in $FILES_TO_COPY )
-{
+foreach ( $FILE in $FILES_TO_COPY ) {
     "Copying file '$($FILE)'..."
-    Copy-item -Force $FILE -Destination "$($TARGET_PACKAGE_DIR_FULL)/$($FILE)"
+    Copy-item -Force $FILE -Destination "$($RESOURCES_DIR)/$($FILE)"
 }
