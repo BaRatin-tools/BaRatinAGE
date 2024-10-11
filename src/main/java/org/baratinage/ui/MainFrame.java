aff4a76d81363b31cffb4898b6be80221f8dc8ec
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2024-10-11 16:38:22 +0200

    added global app shortcut F12 to toggle debug menu

--- src/main/java/org/baratinage/ui/MainFrame.java
+++ src/main/java/org/baratinage/ui/MainFrame.java
@@ -1,6 +1,9 @@
 package org.baratinage.ui;
 
+import javax.swing.AbstractAction;
 import javax.swing.JFrame;
+import javax.swing.JRootPane;
+import javax.swing.KeyStroke;
 
 import org.baratinage.AppSetup;
 import org.baratinage.project_importer.BaratinageV2Importer;
@@ -16,8 +19,10 @@ import org.baratinage.utils.Misc;
 import org.baratinage.utils.perf.TimedActions;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -109,6 +114,8 @@ public class MainFrame extends JFrame {
 
         noProjectPanel = new NoProjectPanel();
 
+        setAppWideShortcuts();
+
         setVisible(true);
         setCurrentProject(null);
 
@@ -123,6 +130,20 @@ public class MainFrame extends JFrame {
             currentProject.updateUI();
         }
         mainMenuBar.updateUI();
+    }
+
+    private void setAppWideShortcuts() {
+        JRootPane rootPane = getRootPane();
+        KeyStroke aKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_F12, 0);
+        rootPane.getInputMap().put(aKeyStroke, "toggle_debug_menu");
+        rootPane.getActionMap().put("toggle_debug_menu", new AbstractAction() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mainMenuBar.toggleDebugMenu();
+            }
+
+        });
     }
 
     // FIXME: disabled / currently unused
