72a230564b800721a73356a3680f9158fcd42496
Ivan Horner <62560360+IvanHeriver@users.noreply.github.com>
2025-02-13 12:39:58 +0100

    refactored SimpleDialog for more genericity and added buildInfoDialog static method

--- src/main/java/org/baratinage/ui/component/SimpleDialog.java
+++ src/main/java/org/baratinage/ui/component/SimpleDialog.java
@@ -8,45 +8,85 @@ import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
 
 import org.baratinage.AppSetup;
 import org.baratinage.translation.T;
 import org.baratinage.ui.container.RowColPanel;
 
-public class SimpleDialog extends JDialog {
+public class SimpleDialog {
 
-    private final RowColPanel modifiableContentPanel;
+    private final JDialog dialog;
+    private final RowColPanel header;
+    private final RowColPanel content;
+    private final RowColPanel footer;
 
-    private SimpleDialog(RowColPanel modifiableContentPanel) {
-        super(AppSetup.MAIN_FRAME, true);
-        this.modifiableContentPanel = modifiableContentPanel;
+    public SimpleDialog(boolean modal) {
+        dialog = new JDialog(AppSetup.MAIN_FRAME, modal);
+        header = new RowColPanel();
+        content = new RowColPanel();
+        footer = new RowColPanel();
+
+        RowColPanel panel = new RowColPanel(RowColPanel.AXIS.COL);
+        panel.setGap(5);
+        panel.setPadding(5);
+        panel.appendChild(header, 0);
+        panel.appendChild(content, 1);
+        panel.appendChild(footer, 0);
+
+        dialog.setContentPane(panel);
     }
 
     public void setSize(Integer width, Integer height) {
-        Dimension dim = getPreferredSize();
+        Dimension dim = dialog.getPreferredSize();
         if (width != null) {
             dim.width = width;
         }
         if (height != null) {
             dim.height = height;
         }
-        setPreferredSize(dim);
+        dialog.setPreferredSize(dim);
     }
 
-    public void updateContent(JPanel newContent) {
-        modifiableContentPanel.clear();
-        modifiableContentPanel.appendChild(newContent, 1);
+    public void setTitle(String title) {
+        dialog.setTitle(title);
+    }
+
+    public void setContent(JComponent newContent) {
+        content.clear();
+        content.appendChild(newContent, 1);
+    }
+
+    public void setHeader(JComponent newHeader) {
+        header.clear();
+        header.appendChild(newHeader, 1);
+    }
+
+    public void setFooter(JComponent newFooter) {
+        footer.clear();
+        footer.appendChild(newFooter, 1);
     }
 
     public void openDialog() {
-        pack();
-        setLocationRelativeTo(AppSetup.MAIN_FRAME);
-        setVisible(true);
+        dialog.pack();
+        dialog.setLocationRelativeTo(AppSetup.MAIN_FRAME);
+        dialog.setVisible(true);
     }
 
-    private static RowColPanel buildActionPanel(JDialog dialog, String okText, ActionListener onOk,
+    public void closeDialog() {
+        dialog.dispose();
+    }
+
+    public static SimpleDialog buildOkCancelDialog(
+            String title,
+            JComponent content,
+            ActionListener onOk,
             ActionListener onCancel) {
+
+        SimpleDialog dialog = new SimpleDialog(true);
+
+        dialog.setTitle(title);
+
+        dialog.setContent(content);
 
         RowColPanel actionsPanel = new RowColPanel();
         actionsPanel.setGap(5);
@@ -54,43 +94,43 @@ public class SimpleDialog extends JDialog {
         cancelButton.setText(T.text("cancel"));
         cancelButton.addActionListener(l -> {
             onCancel.actionPerformed(l);
-            dialog.dispose();
+            dialog.closeDialog();
         });
         JButton okButton = new JButton();
-        okButton.setText(okText);
+        okButton.setText(T.text("ok"));
         okButton.addActionListener(l -> {
             onOk.actionPerformed(l);
-            dialog.dispose();
+            dialog.closeDialog();
         });
         actionsPanel.appendChild(okButton, 1);
         actionsPanel.appendChild(cancelButton, 0);
-        return actionsPanel;
-    }
 
-    public static SimpleDialog buildOkCancelDialog(String title, JComponent component, ActionListener onOk,
-            ActionListener onCancel) {
-        RowColPanel modifiableContentPanel = new RowColPanel();
-        modifiableContentPanel.appendChild(component, 1);
+        dialog.setFooter(actionsPanel);
 
-        SimpleDialog dialog = new SimpleDialog(modifiableContentPanel);
-        RowColPanel panel = new RowColPanel(RowColPanel.AXIS.COL);
-        panel.setGap(5);
-        panel.setPadding(5);
-
-        RowColPanel actionPanel = buildActionPanel(dialog, T.text("ok"), onOk, onCancel);
-
-        panel.appendChild(modifiableContentPanel, 1);
-        panel.appendChild(actionPanel, 0);
-
-        dialog.setContentPane(panel);
-        dialog.addWindowListener(new WindowAdapter() {
+        dialog.dialog.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 onCancel.actionPerformed(null);
             }
         });
 
+        return dialog;
+    }
+
+    public static SimpleDialog buildInfoDialog(String title, JComponent content) {
+
+        SimpleDialog dialog = new SimpleDialog(false);
+
         dialog.setTitle(title);
+
+        JButton closeButton = new JButton();
+        closeButton.setText(T.text("close"));
+        closeButton.addActionListener(l -> {
+            dialog.closeDialog();
+        });
+
+        dialog.setContent(content);
+        dialog.setFooter(closeButton);
 
         return dialog;
     }
