4ff6fde69e985a42b5302b39f6dccc8d27d54bcc
Ivan Horner <contact@ihdev.fr>
2026-01-30 12:03:40 +0100

    Fix not accounting for hidden items when getting domain bounds

--- src/main/java/org/baratinage/ui/plot/Plot.java
+++ src/main/java/org/baratinage/ui/plot/Plot.java
@@ -319,7 +319,7 @@ public class Plot implements IPlot, LegendItemSource {
         for (int k = 0; k < items.size(); k++) {
             PlotItem pi = items.get(k);
             Range r = pi.getDomainBounds();
-            if (r != null) {
+            if (r != null && pi.getVisible()) {
                 if (isLog) {
                     r = findPositiveDomain(pi.getDataset());
                 }
